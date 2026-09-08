f46bc76bf4ed7243bde78268ef373359ee7bb696
Vincente WIlliams <vwilliams2023@fau.edu>
2026-09-08 17:52:18 -0400

    import export

--- Homework1/p1_Williams_Vincente.py
+++ Homework1/p1_Williams_Vincente.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import math
+import matplotlib.pyplot as plt
 
-print("\n" + "="*50)
-=======
-array = [1,2,3,4,5,6];
-
-count = 0
-
-for num in array:
-    count += 1
-print(f"count: {count}");
-
->>>>>>> 9a41eec3667034493167a87e31dcd7abce7d35be
+while True:
+    print("\n" )
