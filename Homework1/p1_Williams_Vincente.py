ac1f2709e414c46bc746c83437228adbda40b40e
Vincente WIlliams <vwilliams2023@fau.edu>
2026-09-08 18:08:41 -0400

    import export

--- Homework1/p1_Williams_Vincente.py
+++ Homework1/p1_Williams_Vincente.py
@@ -1,5 +1,10 @@
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+
+a= float(input("Enter Coefficient a: "))
+b= float(input("Enter Coefficient b: "))
+c= float(input("Enter Coefficient c: "))
 
 while True:
     print("\n" )
